671acaf66a30e6e99b00c9d2afae169c6690e4b2
Maheshwari <97899505+MaheshwariSurwase@users.noreply.github.com>
2022-11-13 01:21:45 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# instagram website
+Developed By Aditya Battin and Maheshwari Surwase
+
+# Features:-
+Login, Signup, Forgot password (with email verification).
+Quick Login (this means app remembers that you had logged in, so you only have enter password to login again).
+Indicates that users exists as you type username when signing up.
+AJAX used almost everywhere (From login to logout).
+Follow, Unfollow, Profile views, Block, Unblock.
+Like, Comment (Text), Share, Remove share, Delete post, Open post.
+Post Text, Images.
+Time ago format used everywhere.
+Edit your profile (From username,Password,Profile Picture).
+Get suggestions on whome to follow.
+Get notified on almost everything from your post being liked, commented,block.
+Create conversation with your followings and give your conversation a title.
+Message Text.
+search users
+Change you password.
+Get all your login details.
+
+# Requirements
+Latest version of PHP.
+
+Thanks for reading
---
+++ instagram.sql
@@ -0,0 +1,159 @@
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+-- Database: `instagram`
+
+-- Table structure for table `block_list`
+
+CREATE TABLE `block_list` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `blocked_user_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- Table structure for table `comments`
+
+CREATE TABLE `comments` (
+  `id` int(11) NOT NULL,
+  `post_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `comment` text NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- Table structure for table `follow_list`
+
+CREATE TABLE `follow_list` (
+  `id` int(11) NOT NULL,
+  `follower_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- Table structure for table `likes`
+
+CREATE TABLE `likes` (
+  `id` int(11) NOT NULL,
+  `post_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- Table structure for table `messages`
+
+CREATE TABLE `messages` (
+  `id` int(11) NOT NULL,
+  `from_user_id` int(11) NOT NULL,
+  `to_user_id` int(11) NOT NULL,
+  `msg` text NOT NULL,
+  `read_status` int(11) NOT NULL DEFAULT 0,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- Table structure for table `notifications`
+
+CREATE TABLE `notifications` (
+  `id` int(11) NOT NULL,
+  `to_user_id` int(11) NOT NULL,
+  `message` text NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `from_user_id` int(11) NOT NULL,
+  `read_status` int(11) NOT NULL DEFAULT 0,
+  `post_id` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- Table structure for table `posts`
+
+CREATE TABLE `posts` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `post_img` text NOT NULL,
+  `post_text` text NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- Table structure for table `users`
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `first_name` varchar(255) NOT NULL,
+  `last_name` varchar(255) NOT NULL,
+  `gender` int(11) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` text NOT NULL,
+  `profile_pic` text NOT NULL DEFAULT 'default_profile.jpg',
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `ac_status` int(11) NOT NULL COMMENT '0=not verified,1=active,2=blocked'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- Indexes for table `block_list`
+ALTER TABLE `block_list`
+  ADD PRIMARY KEY (`id`);
+
+-- Indexes for table `comments`
+ALTER TABLE `comments`
+  ADD PRIMARY KEY (`id`);
+
+-- Indexes for table `follow_list`
+ALTER TABLE `follow_list`
+  ADD PRIMARY KEY (`id`);
+
+-- Indexes for table `likes`
+ALTER TABLE `likes`
+  ADD PRIMARY KEY (`id`);
+
+-- Indexes for table `messages`
+ALTER TABLE `messages`
+  ADD PRIMARY KEY (`id`);
+
+-- Indexes for table `notifications`
+ALTER TABLE `notifications`
+  ADD PRIMARY KEY (`id`);
+
+-- Indexes for table `posts`
+ALTER TABLE `posts`
+  ADD PRIMARY KEY (`id`);
+
+-- Indexes for table `users`
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+
+-- AUTO_INCREMENT for table `block_list`
+ALTER TABLE `block_list`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
+
+-- AUTO_INCREMENT for table `comments`
+ALTER TABLE `comments`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
+
+-- AUTO_INCREMENT for table `follow_list`
+ALTER TABLE `follow_list`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
+
+-- AUTO_INCREMENT for table `likes`
+ALTER TABLE `likes`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
+
+-- AUTO_INCREMENT for table `messages`
+ALTER TABLE `messages`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
+
+-- AUTO_INCREMENT for table `notifications`
+ALTER TABLE `notifications`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
+
+-- AUTO_INCREMENT for table `posts`
+ALTER TABLE `posts`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
+
+-- AUTO_INCREMENT for table `users`
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
+COMMIT;
+
+;
+;
+;
